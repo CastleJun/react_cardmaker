928ec1487f80fd809bc9045777318d86b2fbc2d6
SJ <hsj9360@gmail.com>
2021-08-09 01:07:37 +0900

    add login and logout f

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './footer.module.css';
 const Footer = props => (
   <footer className={styles.footer}>
-    <p className={styles.title}>Code your dream</p>
+    <p className={styles.title}>footer</p>
   </footer>
 );
 
