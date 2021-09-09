1a04c7cebcb43e72af42a3660bb7942bc782d09b
SJ <hsj9360@gmail.com>
2021-09-10 02:00:07 +0900

    add card and edit form component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,20 +1,55 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import Editor from '../editor/editor';
-import Footer from '../footer/footer';
-import Header from '../header/header';
-import Preview from '../preview/preview';
-import styles from './maker.module.css';
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import Editor from "../editor/editor";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+import Preview from "../preview/preview";
+import styles from "./maker.module.css";
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "SJ",
+      company: "wisewedding",
+      theme: "dark",
+      title: "Frontend-developer",
+      email: "hsj9360@gmail.com",
+      message: "go for it",
+      fileName: "SJ",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "SJ2",
+      company: "wisewedding",
+      theme: "light",
+      title: "Frontend-developer",
+      email: "hsj9360@gmail.com",
+      message: "go for it",
+      fileName: "SJ",
+      fileURL: "SJ.png",
+    },
+    {
+      id: "3",
+      name: "SJ3",
+      company: "wisewedding",
+      theme: "colorful",
+      title: "Frontend-developer",
+      email: "hsj9360@gmail.com",
+      message: "go for it",
+      fileName: "SJ",
+      fileURL: null,
+    },
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
   };
 
   useEffect(() => {
-    authService.onAuthChange(user => {
+    authService.onAuthChange((user) => {
       if (!user) {
-        history.push('/');
+        history.push("/");
       }
     });
   });
@@ -22,8 +57,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
